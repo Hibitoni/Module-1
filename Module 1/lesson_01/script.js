0d21152e34fbbfb8f227de65fd12ab7388539427
Даниил Корзун <hibitoni@gmail.com>
2022-10-19 23:32:46 +0500

    add new product

--- Module 1/lesson_01/script.js
+++ Module 1/lesson_01/script.js
@@ -1,12 +1,19 @@
 'use strict';
+{
+    const positionName = 'Choclate Alenka';
+    const quantityGoods = 100;
+    const goodCategory = 'Sweet goods';
+    const goodsPrice = 115;
+    console.log(goodsPrice);
 
-const positionName = 'Choclate Alenka';
-const quantityGoods = 100;
-const goodCategory = 'Sweet goods';
-const goodsPrice = 115;
-console.log(goodsPrice);
+    const dopZadanie = goodsPrice * quantityGoods;
+    console.log('Общая сумма всех имеющихся шоколадок равна:', dopZadanie);
+}
 
-const dopZadanie = goodsPrice * quantityGoods;
-console.log('Общая сумма всех имеющихся шоколадок равна:', dopZadanie);
-
-
+{
+    const positionName_1 = 'Bubble Gum'
+    const quantityGoods = 200;
+    const goodCategory = 'For kids';
+    const goodsPrice = 25;
+    console.log(positionName_1);
+}
