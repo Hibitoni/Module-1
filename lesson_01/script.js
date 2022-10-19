c1d804e680d7ba36ed91e8695744bda0fad64a21
Даниил Корзун <hibitoni@gmail.com>
2022-10-19 22:44:39 +0500

    initial

---
+++ lesson_01/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const positionName = 'Choclate Alenka';
+const quantityGoods = 100;
+const goodCategory = 'Sweet goods';
+const goodsPrice = 115;
+console.log(goodsPrice);
+
+const dopZadanie = goodsPrice * quantityGoods;
+console.log('Общая сумма всех имеющихся шоколадок равна:', dopZadanie);
+
+
