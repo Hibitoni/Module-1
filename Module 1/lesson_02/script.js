acc172fe6f6e24d9ca09ede111ed4573598af2ff
Даниил Корзун <hibitoni@gmail.com>
2022-10-22 00:45:26 +0500

    -m add things

--- Module 1/lesson_02/script.js
+++ Module 1/lesson_02/script.js
@@ -1,4 +1,19 @@
 'use strict';
+
+{
+    const newName = prompt('Введите наименование товара');
+    const newCount = parseInt(prompt('Введите количество товара'));
+
+    console.log('newCount is:', typeof(newCount));
+
+    const newCategory = prompt('Введите категорию товара');
+    const newPrice = parseInt(prompt('Введите стоимость товара'));
+    console.log('newPrice is: ', typeof(newPrice));
+    const umnozhenie = newCount * newPrice;
+    console.log(`На складе ${newCount} единицы товара "${newName}" на сумму ${umnozhenie} деревянных`);
+}
+
+/** 
 {
     const positionName = 'Choclate Alenka';
     const quantityGoods = 100;
@@ -17,3 +32,4 @@
     const goodsPrice = 25;
     console.log(positionName_1);
 }
+*/
