19a1f910998656dfcd9db0157712f07a3cdd9678
Даниил Корзун <hibitoni@gmail.com>
2022-10-23 01:41:01 +0500

    additionalTasks

--- Module 1/lesson_03/taskHard_1.js
+++ Module 1/lesson_03/taskHard_1.js
@@ -1,14 +1,13 @@
 'use strict';
-/*
-{
-    const netWort = Number(prompt('Укажите Ващ доход:'));
 
-    if (netWort < 15000) {
-        console.log('Tax summ', Math.round(netWort * 0.13));    //Округляется просто потому что в пользу Государства С:
-    } else if (15000 < netWort < 50000) {
-        console.log('Tax summ', Math.round(netWort * 0.2));    //Округляется просто потому что в пользу Государства С:
-    } else if (netWort > 50000) {
-        console.log('Tax summ', Math.round(netWort * 0.3));    //Округляется просто потому что в пользу Государства С:
+{
+    const netWort1 = Number(prompt('Укажите Ващ доход:'));
+
+    if (netWort1 < 15000) {
+        console.log('Tax summ', Math.round(netWort1 * 0.13));    //Округляется просто потому что в пользу Государства С:
+    } else if (15000 < netWort1 < 50000) {
+        console.log('Tax summ', Math.round(netWort1 * 0.2));    //Округляется просто потому что в пользу Государства С:
+    } else if (netWort1 > 50000) {
+        console.log('Tax summ', Math.round(netWort1 * 0.3));    //Округляется просто потому что в пользу Государства С:
     }
 }
-*/
