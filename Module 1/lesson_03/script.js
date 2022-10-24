2136052bc909680cf149f705b8a682de1ee8d0a4
Даниил Корзун <hibitoni@gmail.com>
2022-10-24 22:23:41 +0500

    rename folders

--- Module 1/lesson_03/script.js
+++ Module 1/lesson_03/script.js
@@ -1,20 +1,35 @@
 'use strict';
 
 {
-    //const newName = prompt('Введите наименование товара');
-    const newCount = Number(prompt('Введите количество товара'));
-    console.log('newCount: ', typeof(newCount));
-    if (Number.isNaN(newCount) === 'true') {     //Я понимаю что это может быть не совсем верным сравнением, но  Метод parseint, как я понял, всегда возвращает тип данных Number
-    } else {                                // Даже если введены буквы. Тогда как правильно преобразовать строку в число, чтобы потом типы данных можно было сравнить?
-        console.log('Вы ввели некорректное количество товара');
-    }
+    const newName = prompt('Введите наименование товара');
+    const newCount = parseInt(prompt('Введите количество товара'));
 
+    console.log('newCount is:', typeof(newCount));
 
-    //const newCategory = prompt('Введите категорию товара');
+    const newCategory = prompt('Введите категорию товара');
     const newPrice = parseInt(prompt('Введите стоимость товара'));
-    if (typeof(newPrice) === 'number') {
-    } else {  
-        console.log('Вы ввели некорректное количество товара');
-    }
-
+    console.log('newPrice is: ', typeof(newPrice));
+    const umnozhenie = newCount * newPrice;
+    console.log(`На складе ${newCount} единицы товара "${newName}" на сумму ${umnozhenie} деревянных`);
 }
+
+/** 
+{
+    const positionName = 'Choclate Alenka';
+    const quantityGoods = 100;
+    const goodCategory = 'Sweet goods';
+    const goodsPrice = 115;
+    console.log(goodsPrice);
+
+    const dopZadanie = goodsPrice * quantityGoods;
+    console.log('Общая сумма всех имеющихся шоколадок равна:', dopZadanie);
+}
+
+{
+    const positionName_1 = 'Bubble Gum'
+    const quantityGoods = 200;
+    const goodCategory = 'For kids';
+    const goodsPrice = 25;
+    console.log(positionName_1);
+}
+*/
---
+++ Module 1/lesson_04/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+    //const newName = prompt('Введите наименование товара');
+    const newCount = Number(prompt('Введите количество товара'));
+    console.log('newCount: ', typeof(newCount));
+    if (Number.isNaN(newCount) === 'true') {     //Я понимаю что это может быть не совсем верным сравнением, но  Метод parseint, как я понял, всегда возвращает тип данных Number
+    } else {                                // Даже если введены буквы. Тогда как правильно преобразовать строку в число, чтобы потом типы данных можно было сравнить?
+        console.log('Вы ввели некорректное количество товара');
+    }
+
+
+    //const newCategory = prompt('Введите категорию товара');
+    const newPrice = parseInt(prompt('Введите стоимость товара'));
+    if (typeof(newPrice) === 'number') {
+    } else {  
+        console.log('Вы ввели некорректное количество товара');
+    }
+
+}
