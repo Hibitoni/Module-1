f3f9001b491f546c89daf45e42d42649270f68d4
Даниил Корзун <hibitoni@gmail.com>
2022-11-27 15:42:21 +0500

    initial

---
+++ Module 1/lesson_08/Task04/Task04.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const getLeapYears = (start, end) =>
+Array.from({ length: end - start + 1 }, (n, i) => start + i)
+  .filter(n => ((n % 100) && !(n % 4)) || !(n % 400));
+
+  
+console.log(getLeapYears(1900, 2022));
